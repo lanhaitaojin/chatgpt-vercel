a725959302d2adc6a3ae0726b9b9a6f2697b9fb2
leo <130837051+lanhaitaojin@users.noreply.github.com>
2023-07-18 16:50:49 +0800

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -18,7 +18,7 @@ export const defaultEnv = {
     continuousDialogue: true,
     APIModel: "gpt-3.5-turbo" as Model
   },
-  CLIENT_DEFAULT_MESSAGE: `顼龙科技打造
+  CLIENT_DEFAULT_MESSAGE: `
 - 开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。
 - [[↑]] 可编辑最近一次提问。
 - 点击顶部名称滚动到顶部，点击输入框滚动到底部。
